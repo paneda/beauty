19cc9c40d34e68f73cb15c43631fb46abbe7d08a
jsc <jorgen.scott@paneda.se>
2025-09-03 07:06:28 +0200

    Updates status line version to HTTP/1.1

--- src/reply.cpp
+++ src/reply.cpp
@@ -6,23 +6,23 @@ namespace beauty {
 
 namespace status_strings {
 
-const std::string ok = "HTTP/1.0 200 OK\r\n";
-const std::string created = "HTTP/1.0 201 Created\r\n";
-const std::string accepted = "HTTP/1.0 202 Accepted\r\n";
-const std::string no_content = "HTTP/1.0 204 No Content\r\n";
-const std::string multiple_choices = "HTTP/1.0 300 Multiple Choices\r\n";
-const std::string moved_permanently = "HTTP/1.0 301 Moved Permanently\r\n";
-const std::string moved_temporarily = "HTTP/1.0 302 Moved Temporarily\r\n";
-const std::string not_modified = "HTTP/1.0 304 Not Modified\r\n";
-const std::string bad_request = "HTTP/1.0 400 Bad Request\r\n";
-const std::string unauthorized = "HTTP/1.0 401 Unauthorized\r\n";
-const std::string forbidden = "HTTP/1.0 403 Forbidden\r\n";
-const std::string not_found = "HTTP/1.0 404 Not Found\r\n";
-const std::string internal_server_error = "HTTP/1.0 500 Internal Server Error\r\n";
-const std::string not_implemented = "HTTP/1.0 501 Not Implemented\r\n";
-const std::string bad_gateway = "HTTP/1.0 502 Bad Gateway\r\n";
-const std::string service_unavailable = "HTTP/1.0 503 Service Unavailable\r\n";
-const std::string version_not_supported = "HTTP/1.0 505 Version Not Supported\r\n";
+const std::string ok = "HTTP/1.1 200 OK\r\n";
+const std::string created = "HTTP/1.1 201 Created\r\n";
+const std::string accepted = "HTTP/1.1 202 Accepted\r\n";
+const std::string no_content = "HTTP/1.1 204 No Content\r\n";
+const std::string multiple_choices = "HTTP/1.1 300 Multiple Choices\r\n";
+const std::string moved_permanently = "HTTP/1.1 301 Moved Permanently\r\n";
+const std::string moved_temporarily = "HTTP/1.1 302 Moved Temporarily\r\n";
+const std::string not_modified = "HTTP/1.1 304 Not Modified\r\n";
+const std::string bad_request = "HTTP/1.1 400 Bad Request\r\n";
+const std::string unauthorized = "HTTP/1.1 401 Unauthorized\r\n";
+const std::string forbidden = "HTTP/1.1 403 Forbidden\r\n";
+const std::string not_found = "HTTP/1.1 404 Not Found\r\n";
+const std::string internal_server_error = "HTTP/1.1 500 Internal Server Error\r\n";
+const std::string not_implemented = "HTTP/1.1 501 Not Implemented\r\n";
+const std::string bad_gateway = "HTTP/1.1 502 Bad Gateway\r\n";
+const std::string service_unavailable = "HTTP/1.1 503 Service Unavailable\r\n";
+const std::string version_not_supported = "HTTP/1.1 505 Version Not Supported\r\n";
 
 asio::const_buffer toBuffer(Reply::status_type status) {
     switch (status) {
